3949f5cb0bfd52995aa70ad47438c1b4f97b8609
ananth krishnan a <ab.ananth97@gmail.com>
2022-04-19 20:17:46 +0530

    added login validation

--- scripts/script.js
+++ scripts/script.js
@@ -13,13 +13,43 @@ var signup_inputCity = document.getElementById("signup_inputCity");
 var signup_inputZip = document.getElementById("signup_inputZip");
 var signup_gridCheck = document.getElementById("signup_gridCheck");
 var signup_inputState = document.getElementById("signup_inputState");
+
+// starting of regular express
+
 var mobile =/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;   
 var regexp = /^([A-Za-z0-9\.-]+)@([A-Za-z\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/; 
 var zipexp = /^([0-9]{6})$/;
 var regpass = /^(?=.*[A-Za-z])(?=.*\d)[A-za-z0-9]{8,}$/;
 
-function signupvalidation(){
+// end of regular express
+
+// staring of login validation
+
+function loginvalidation(){
+
+    if(loginemail.value == "" || loginpwd == ""){
+        error_message.innerText = "field cannot be empty";
+        return false;
+    }
+
+    if(!regexp.test(loginemail.value)){
+        error_message.innerText = "required email address formate";   
+        return false; 
+      }
+
+      if(!regpass.test(loginpwd.value)){
+        error_message.innerText = "Password doesnot meet";
+        return false;
+    }
     
+}
+
+// end of login validation
+
+// starting of sign up validation
+
+function signupvalidation(){
+
   var state = signup_inputState.options[signup_inputState.selectedIndex].value;
 
   if(signup_firstName.value =="" || signup_lastName.value =="" || signup_inputEmail4.value =="" || signup_inputPassword4.value =="" || signup_confirminputPassword4.value == "" || signup_inputAddress.value == "" || signup_inputAddress2.value == "" || signup_inputCity.value == "" || signup_mobile4.value == "" || signup_inputZip.value == "" || signup_gridCheck.checked == "" || state==0){
@@ -52,3 +82,4 @@ function signupvalidation(){
       return false;
   }       
 }
+// end of sign up validation
